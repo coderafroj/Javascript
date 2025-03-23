05c70d14e4e0dff5e862ae5c69f69d01e4041c2f
coderafroj <coderafroj@gmail.com>
2025-03-23 18:22:42 +0530

    add file

---
+++ Conversion.js
@@ -0,0 +1,17 @@
+let num="5y5";
+let intNum=Number(num) //string convert number
+console.log(intNum)  //NaN --not a number
+console.log(typeof intNum)
+let x=1;
+let True=Boolean(x);
+console.log(True)
+let y=0;
+let False=Boolean(y);
+console.log(False)
+let s="t"
+let F=Boolean(s)
+console.log(F)
+/*
+1---true , 0--false
+""-- false,"afroj"---true
+*/
---
+++ datatypes.js
@@ -0,0 +1,10 @@
+let name="coderafroj"; //String using single and double quetes
+let age=18;           // number
+let time=10.23;
+let course;           //undefined yani bas dwclare kar rakha hai koi value nahi asign ki
+let job=null;          //null yani empty
+let isKid=true;        //boolean
+let isJob=false;
+let mobileNo=7505011567n;  // bigInt
+x="type"
+console.log([name,x,typeof name,age,x,typeof age,course,x,typeof course,job,x,typeof job,isKid,x,typeof isKid,mobileNo,x,typeof mobileNo])
