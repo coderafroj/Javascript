8a0b0b1efa3f5dd68d62f3356cb55f116033342b
coderafroj <coderafroj@gmail.com>
2025-04-05 08:36:39 +0530

    ad

---
+++ Number.js
@@ -0,0 +1,5 @@
+const score=309
+console.log(score.toFixed(2))//-point ke badd zero
+console.log(score.toString())//--convert string
+const num=new Number(100) 
+console.log(num)
--- string.js
+++ string.js
@@ -11,3 +11,10 @@ console.log(n.charAt(4))//--char return karta hai index number se
 //console.log(n.substring(1,6))
 console.log(n)
 console.log(n.slice(-9,-1))
+const string="  coderafroj  ";
+console.log(string.trim())
+const str="java is a interprated orogramming language"
+console.log(str.replace("java","python"))
+console.log(str.includes("python"))
+console.log(str.split(" "))
+
