3b2531964f8eb06a420f800153f233088d90db3a
coderafroj <coderafroj@gmail.com>
2025-04-15 08:38:19 +0530

    add

---
+++ iteration/For.js
@@ -0,0 +1,36 @@
+//for loop
+// for(let i=0;i<10;i++){
+//   const element=i
+//   console.log(element)
+// }
+
+// for(let i=0;i<=10;i++){
+//   if(i%2==0){
+//     console.log(`${i} this is even number`)
+//   }
+//   console.log(i)
+// }
+
+// for(let i=0;i<10;i++){
+//   console.log(`outer loop ${i}`)
+//   for(let j=0;j<10;j++){
+//     console.log(`inner loop value ${j} inner loop ${i}`)
+//   }
+// }
+
+//table
+for(let i=1;i<=10;i++){
+  // console.log(`table of ${i}`)
+  for(let j=1;j<=10;j++){
+    // console.log(`${i}*${j} = ${i*j}`)
+  }
+}
+const myArray=["Python","Java","Javascript","html/css"]
+for(let i=0;i<myArray.length;i++){
+  const el=myArray[i]
+ // console.log(el)
+}
+
+//break and continue
+
+
